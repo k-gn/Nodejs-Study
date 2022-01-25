51dd654ed4e215f57983ab107ab086b7ad2d9ce3
k-gn <rlarbska97@gmail.com>
2022-01-25 22:50:58 +0900

    no message

---
+++ zerocho_node_lecture/ch_1/func.js
@@ -0,0 +1,24 @@
+const value = require('./var');
+
+const {odd, even} = require('./var');
+
+console.log(value);
+
+function check(number) {
+
+    if(number % 2) {
+        return odd;
+    }else {
+        return even;
+    }
+}
+
+const result = check(5);
+console.log(result);
+
+// 파일에서 단 한번만 사용
+module.exports = {
+    check,
+    odd,
+    even
+}
---
+++ zerocho_node_lecture/ch_1/hello.js
@@ -0,0 +1,5 @@
+function helloNode() {
+    console.log("hello node!");
+}
+
+helloNode();
