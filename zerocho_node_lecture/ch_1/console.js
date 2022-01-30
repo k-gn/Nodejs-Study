2552549c828c4f33868ab7773c35bcf4137160fc
k-gn <rlarbska97@gmail.com>
2022-01-30 13:58:42 +0900

    no message

---
+++ zerocho_node_lecture/ch_1/console.js
@@ -0,0 +1,13 @@
+console.dir({name : 'hello'});
+
+const obj = {name : 'helle', age : 20};
+console.dir(obj);
+
+
+console.time("zero");
+console.error("error");
+console.timeEnd("zero");
+
+console.trace("에러 추적");
+
+console.table(obj);
